72e23d2051d44fd2d4362d9013c56e3c462029da
gunmother <gunmother@gmail.com>
2025-09-01 16:21:19 -0600

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+// Name of the cache
+const CACHE_NAME = "chrono-cache-v1";
+
+// Files to cache (start with just your index)
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html"
+];
+
+// Install event - cache files
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate event - cleanup old caches
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch event - serve from cache first
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
